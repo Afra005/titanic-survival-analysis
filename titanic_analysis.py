012cf977f59ec912e22524a4255a65c2524e82fc
Afra005 <redoxafra@gmail.com>
2026-09-22 12:50:29 +0600

    titanic_analysis.py

--- titanic_analysis.py
+++ titanic_analysis.py
@@ -66,7 +66,7 @@ plt.title("Age distribution of titanic passengers")
 plt.tight_layout()
 plt.show()
 ##Visualization 4 — Survival by Age
-survival_by_age = df.groupby("AgeGroup", observed=True)["Survived"].mean()
+survival_by_age = df.groupby("AgeGroup", observed=True)["Survived"].mean()*100
 print(survival_by_age)
 survival_by_age.plot(kind="bar")
 plt.xlabel("Age")
